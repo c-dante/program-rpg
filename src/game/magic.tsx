0af81405e38598e9a0c9ea3894e0efee6cd06730
dante <c.dante.federici@gmail.com>
2021-02-28 00:49:29 -0500

    Use single quote

--- src/game/magic.tsx
+++ src/game/magic.tsx
@@ -1,20 +1,25 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial, Vector3 } from "three";
-import { TimeStep } from "./actor";
-import { ContextApi } from "./api";
-import { SCALE, Colors, Tag } from "./config";
+import { BoxGeometry, Mesh, MeshBasicMaterial, Vector3, Spherical } from 'three';
+import { TimeStep } from './actor';
+import { ContextApi } from './api';
+import { SCALE, Colors, Tag } from './config';
 
-export type SpellFn = (_: typeof Vector3) => (delta: number, position: Vector3, velocity: Vector3) => void;
+const SpellEnv = {
+	Vector3,
+	Spherical,
+};
+
+export type SpellFn = (_: typeof SpellEnv) => (delta: number, position: Vector3, velocity: Vector3) => void;
 
 export type Spell = {
 	source: string,
 	fn: SpellFn
 };
 
-export const invokeNewSpell = (spell: SpellFn) => spell(Vector3);
+export const invokeNewSpell = (spell: SpellFn) => spell(SpellEnv);
 
 export const compileSpell = (incantation: string) => {
 	// eslint-disable-next-line no-new-func
-	const fn = new Function(`"use strict";return (Vector3) => { ${incantation} }`)() as SpellFn;
+	const fn = new Function(`"use strict";return ({ Vector3, Spherical }) => { ${incantation} }`)() as SpellFn;
 
 	// Ensure we can evoke
 	invokeNewSpell(fn);
@@ -29,27 +34,27 @@ export const compileSpell = (incantation: string) => {
 export const spellBook: Spell[] = [
 	compileSpell(
 `return (delta, position, velocity) => {
-  position.add(velocity.multiplyScalar(delta));
+  position.addScaledVector(velocity, delta);
 };`),
 	compileSpell(
 `let theta = 0;
 const up = new Vector3(0.0, 0.0, 1.0);
 return (delta, position, velocity) => {
-  position.add(velocity.multiplyScalar(delta));
+  position.addScaledVector(velocity, delta);
   velocity.applyAxisAngle(up, theta);
   theta += 0.0001;
 };`),
-	compileSpell(
-	`const up = new Vector3(0.0, 0.0, 1.0);
-	const theta = Math.random() * Math.PI * 2;
-	const fixedVelocity = new Vector3(1.0, 1.0, 0.0)
-		.applyAxisAngle(up, theta)
-		.normalize()
-		.multiplyScalar(0.03);
+compileSpell(
+`const up = new Vector3(0.0, 0.0, 1.0);
+const theta = Math.random() * Math.PI * 2;
+const fixedVelocity = new Vector3(1.0, 1.0, 0.0)
+	.applyAxisAngle(up, theta)
+	.normalize()
+	.multiplyScalar(0.03);
 
-	return (delta, position, velocity) => {
-		position.add(fixedVelocity.multiplyScalar(delta));
-	};`)
+return (delta, position, velocity) => {
+	position.addScaledVector(fixedVelocity, delta);
+};`),
 ];
 
 export const spellCaster = (spellLogic: Spell = spellBook[0]) => {
