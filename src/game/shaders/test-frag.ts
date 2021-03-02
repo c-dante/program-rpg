0211c3bf28b859cb492f2b60a6743b32aa7fda4e
dante <c.dante.federici@gmail.com>
2021-03-02 01:54:33 -0500

    Playing with shaders

--- src/game/shaders/test-frag.ts
+++ src/game/shaders/test-frag.ts
@@ -1,35 +1,31 @@
 const source = `varying vec2 vUv;
 
+// rg gradient
+vec4 simple_gradient(vec2 position, vec2 gridcell) {
+	return vec4(
+		position.yx * 100.0,
+		1.0,
+		1.0
+	);
+}
+
+
+vec4 ground(vec2 position, vec2 gridcell) {
+	return vec4(gridcell.xy, 1.0, 1.0);
+}
+
+
+// Simple grid overlay combine
 void main( void ) {
 	vec2 position = - 1.0 + 2.0 * vUv;
+	vec2 gridcell = mod(floor(position.xy * 100000.0), 80.0) / 80.0;
+	vec4 color = simple_gradient(position, gridcell);
 
-	// float red = abs( sin( position.x * position.y ) );
-	// float green = abs( sin( position.x * position.y ) );
-	// float blue = abs( sin( position.x * position.y ) );
-	// gl_FragColor = vec4( red, green, blue, 1.0 );
-
-	// if (
-	// 	mod(floor(vUv.x * 100000.0), 100.0) == 0.
-	// 	|| mod(floor(vUv.y* 100000.0), 100.0) == 0.
-	// ) {
-	// 		gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 );
-	// } else {
-	// 		gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );
-	// }
-
-	if (
-		mod(floor(position.x * 100000.0), 80.0) == 0.0
-		|| mod(floor(position.y * 100000.0), 80.0) == 0.0
-	) {
-		gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+	if (gridcell.x == 0.0 || gridcell.y == 0.0) {
+		gl_FragColor = -color;
 	} else {
-		gl_FragColor = vec4(
-			position.yx * 100.0,
-			1.0,
-			1.0
-		);
+		gl_FragColor = color;
 	}
-
 }`;
 
 export default source;
